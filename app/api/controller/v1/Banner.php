a2cf0274a71cbfd163af9fc9760da5af1bfaf381
junzhou1999 <outtatouch2020@126.com>
2022-11-19 22:17:37 +0800

    TP6采取多应用并添加api应用下的路由定义

---
+++ app/api/controller/v1/Banner.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\api\controller\v1;
+
+class Banner
+{
+  /**
+   * 根据id号获取id信息
+   * @return string
+   */
+  public function getBanner($id) {
+    return 'res';
+  }
+
+}
---
+++ app/api/route/route.php
@@ -0,0 +1,5 @@
+<?php
+
+use think\facade\Route;
+
+Route::get('v1/banner/:id', 'v1.Banner/getBanner');
