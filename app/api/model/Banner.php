3cd93a40d21576fea963588ba095683bb374a279
junzhou1999 <outtatouch2020@126.com>
2022-11-21 20:38:15 +0800

    使用构造器执行sql语句

--- app/api/model/Banner.php
+++ app/api/model/Banner.php
@@ -12,7 +12,21 @@ class Banner
    * @return
    */
   public static function getBannerByID($id) {
-    $res = Db::query('select * from banner_item where banner_id = ?', [$id]);
+    // 原生sql查询
+    // $res = Db::query('select * from banner_item where banner_id = ?', [$id]);
+
+    // 链式查询
+//    $res = Db::table('banner_item')
+//      ->where(function ($query) use ($id) {   // where(可以用表达式，也可以用闭包的方式)
+//        $query->where('banner_id', '=', $id);
+//      })
+//      ->select();
+
+    // 链式查询
+    $res = Db::table('banner_item')
+      ->where('banner_id', '=', $id)  // 返回Query对象，不会立即查询数据库
+      ->select();
+
     return $res;
   }
 }
