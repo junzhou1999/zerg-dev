6b6aee63cbd8ed8cc251187b6b28545539c46cfe
junzhou1999 <outtatouch2020@126.com>
2022-11-26 21:38:49 +0800

    生成订单前实现订单快照的存储

--- app/api/service/Order.php
+++ app/api/service/Order.php
@@ -3,18 +3,20 @@
 namespace app\api\service;
 
 use app\api\model\Product as ProductModel;
+use app\api\model\UserAddress as UserAddressModel;
 use app\lib\exception\OrderException;
+use app\lib\exception\WechatUserException;
 
 class Order
 {
   protected $oProducts;    // 客户端传进来的商品信息
-  protected $ProductsDb;   // 真实的商品信息
+  protected $productsDb;   // 真实的商品信息
   protected $uid;          // 用户id
 
   public function place($uid, $oProducts) {
     $this->uid = $uid;
     $this->oProducts = $oProducts;
-    $this->ProductsDb = $this->getProductsByOrder($this->oProducts);
+    $this->productsDb = $this->getProductsByOrder($this->oProducts);
 
     $status = $this->getOrderStatus();
     if (!$status['pass']) {
@@ -23,6 +25,53 @@ class Order
     }
 
     // 开始创建订单
+    $orderSnap = $this->snapOrder($status);
+  }
+
+  /**
+   * 生成订单快照，查询订单的信息需要生成历史快照，直接去数据库的数据会导致数据更新不吻合
+   * @return void
+   */
+  private function snapOrder($status) {
+    // 业务需求所需要的信息
+    $snap = [
+      'orderPrice' => 0,  // 订单总价格
+      'totalCount' => 0,  // 商品总数量，不是订单商品数量
+      'pStatus' => [],    // 各个商品的状态
+      'snapAddress' => null,  // 订单收货地址
+      'snapName' => '',   // 订单标题
+      'snapImg' => '',    // 订单图片
+    ];
+
+    $snap['orderPrice'] = $status['order_price'];
+    $snap['totalCount'] = $status['totalCount'];
+    $snap['pStatus'] = $status['pStatusArray'];
+    // 数组作为字符串放进表字段，索引查找麻烦/建新表？关系复杂/选择nosql文档性数据库？mongodb
+    $snap['snapAddress'] = json_encode($this->getUserAddress($this->uid));
+    // 选择第一个商品作为快照的标题，图片
+    $snap['snapName'] = $this->productsDb[0]['name'];
+    $snap['snapImg'] = $this->productsDb[0]['main_img_url'];
+
+    // 明晰订单标题
+    if (count($this->productsDb) > 1) {
+      $snap['snapName'] .= '等';
+    }
+    return $snap;
+  }
+
+  /**
+   * 查询用户地址
+   */
+  private function getUserAddress($uid) {
+    $userAddress = UserAddressModel::where('user_id', '=', $uid)
+      ->find();
+    if (!$userAddress) {
+      throw new WechatUserException([
+        'message' => '用户收获地址不存在，下单失败',
+        'status_code' => 60001
+      ]);
+      return $userAddress->toArray();
+    }
   }
 
   /**
@@ -31,17 +80,20 @@ class Order
   private function getOrderStatus() {
     // 二维数组
     $status = [
-      'pass' => true,
-      'order_price' => 0,
-      'pStatusArray' => []
+      'pass' => true,       // 订单总的可支付状态
+      'order_price' => 0,   // 订单总价格
+      'totalCount' => 0,    // 订单总商品数量
+      'pStatusArray' => []  // 每个订单支付商品的信息
     ];
     foreach ($this->oProducts as $product) {
       // 获取每个订单商品支付状态
       $pStatus = $this->getProductStatus($product['product_id'],
-        $product['count'], $this->ProductsDb);
+        $product['count'], $this->productsDb);
       // 一个商品没有库存的话，订单失效
-      if (!$pStatus['haveStock']) $pStatus['pass'] = false;
+      if (!$pStatus['haveStock'])
+        $pStatus['pass'] = false;
       $status['order_price'] += $pStatus['totalPrice'];
+      $status['totalCount'] += $pStatus['count'];
       array_push($status['pStatusArray'], $pStatus);
     }
     return $status;
