bf7bc34f02b100fc5074ba0a605669523ecd6cc7
scott <ousttatouch2020@126.com>
2024-01-18 10:33:25 +0800

    服务端全局允许跨域

---
+++ app/api/middleware/CORS.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\api\middleware;
+
+class CORS
+{
+  // 前置全局解决跨域
+  public function handle($request, \Closure $next) {
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: token,Origin, X-Requested-With, Content-Type, Accept");
+    header('Access-Control-Allow-Methods: POST,GET');
+    if ($request->isOptions()) {
+      return response()->code(204);
+    }
+
+    return $next($request);  // 前置中间件
+  }
+}
--- app/middleware.php
+++ app/middleware.php
@@ -7,4 +7,7 @@ return [
   // \think\middleware\LoadLangPack::class,
   // Session初始化
   // \think\middleware\SessionInit::class
+
+  // 跨域
+  \app\api\middleware\CORS::class
 ];
