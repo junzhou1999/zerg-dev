c5d360ba4074c097f22f2e99c7a722d5887c38e4
scott <ousttatouch2020@126.com>
2024-02-04 11:31:27 +0800

    跨域仅限于admin应用

--- app/admin/middleware.php
+++ app/admin/middleware.php
@@ -1,5 +1,6 @@
 <?php
 // 这是系统自动生成的middleware定义文件
 return [
-
+  // 跨域
+  \app\admin\middleware\CORS::class
 ];
--- app/admin/middleware/CORS.php
+++ app/admin/middleware/CORS.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace app\api\middleware;
+namespace app\admin\middleware;
 
 class CORS
 {
